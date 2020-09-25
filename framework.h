5e5916748e72cc7e6438d48d2b549e165915d5f1
Potato <30365474+2544632b@users.noreply.github.com>
2020-09-25 15:18:43 +0800

    Create framework.h

---
+++ framework.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+#include <tchar.h>
+#include <iostream>
+#include <time.h>
+#include <locale.h>
+#include <direct.h>
+#include "pch.h"
