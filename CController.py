57959b5610bb747cf1c5cff03e2301f8a7b8e174
Potato <30365474+2544632b@users.noreply.github.com>
2020-09-25 15:08:04 +0800

    DLL load updated
    DLL load updated

--- CController.py
+++ CController.py
@@ -1,23 +1,45 @@
 import ctypes
 import os
 import sys
+import threading
+import time
+import socket
 
-# You may compile the python program as c binary.
-cdef ProcessServer():	# Run as threading
-	pDll = ctypes.WinDLL("./CustomProcessDymaicLibrary.dll")
+# GameConsole simple
+def ProcessServer():	# Run as threading
 	print(pDll)
 	print "[Launcher] Processing server"
 	pDll.Startup_Server()
 
-cdef PoweroffServer():	# Run as threading
-	pDll = ctypes.WinDLL("./CustomProcessDymaicLibrary.dll")
+def PoweroffServer():	# Run as threading
 	print(pDll)
 	print "[Launcher] Poweroffed server"
 	pDll.Poweroff_Server()
 
-cdef void_RestartServer():	# This is a virtual action
-	pDll = ctypes.WinDLL("./CustomProcessDymaicLibrary.dll")
+def void_RestartServer():	# This is a virtual action
 	print(pDll)
 	print "[Launcher] Restarting server"
 	pDll.Poweroff_Server()
 	pDll.Startup_Server()
+
+def socket_application():
+	networkd = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	networkd.bind(('127.0.0.1', 6500))
+	networkd.listen(10000)
+	print "Server is listening on %s:%s"%("127.0.0.1", 6500)
+	while 1:
+		conn, addr = networkd.accept()
+		data = conn.recv(2048)
+		
+		if(data == b'\x00\x0f\x00\x00'):
+			threading.Thread(target=ProcessServer).start()
+			conn.sendall(b'\x00\x0f\x01\x00')
+
+		if(data == b'\x00\x0f\x00\x01'):
+			threading.Thread(target=PoweroffServer).start()
+			conn.send(b'\x00\x0f\x01\x01')
+
+		conn.close()
+
+pDll = ctypes.WinDLL("./CustomProcessDymaicLibrary.dll")	#Override pDll
+socket_application()
